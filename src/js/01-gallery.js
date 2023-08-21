01b84f8308955650f51ab71f8eccfc3d0dbb830d
Przemyslaw Daniel Lapicz <przemyslaw.lapicz@gmail.com>
2023-08-21 18:49:51 +0200

    add changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const liItem = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+  </li>
+`;
+
+const getImages = img => img.map(item => liItem(item)).join('');
+
+gallery.innerHTML = getImages(galleryItems);
+
+gallery.addEventListener('click', onClickGallery);
+
+// utworzenie instancji: podpis pod obrazkiem po czasie 250 ms
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onClickGallery(e) {
+  e.preventDefault();
+}
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
+import PLayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', data => {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(savedTime);
+
+function updateTime(currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const throttledUpdateTime = trottle(updateTime, 1000);
+
+player.on('timeupdate', data => {
+  const currentTime = data.seconds;
+  throttledUpdateTime(currentTime);
+});
+
+function checkSavedTime() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  player.getDuration().then(duration => {
+    if (savedTime === null || savedTime > duration) {
+      player.setCurrentTime(0);
+    }
+  });
+}
+
+player.on('loaded', () => {
+  checkSavedTime();
+});
+
+player.on('play', () => {
+  checkSavedTime();
+});
+
+player.on('ended', () => {
+  player.setCurrentTime(0);
+});
